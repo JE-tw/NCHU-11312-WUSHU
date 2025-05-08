f835ef485d9e873f1d45a0454450b2982c6a0941
Huiqi <rf259242396@gmail.com>
2025-05-08 11:23:37 +0800

    BackToTop組件調整
    修改手機板RWD

--- resources/js/components/BackToTop.vue
+++ resources/js/components/BackToTop.vue
@@ -24,14 +24,15 @@ const scrollToTop = () => {
 </script>
 
 <template>
-    <button v-show="showButton" @click="scrollToTop" :class="[
-        'fixed z-[9999] rounded-full shadow-md bg-transparent',
-        'w-[80px] h-[80px] bottom-6 right-6',           
-        'sm:w-[80px] sm:h-[80px] sm:bottom-6 sm:right-6', 
-        'w-[50px] h-[50px] bottom-4 right-4'             
-    ]">
+    <button v-show="showButton" @click="scrollToTop" 
+        :class="[
+            'fixed z-[9999] rounded-full shadow-md bg-transparent',
+            'w-[50px] h-[50px] bottom-4 right-4',
+            'sm:w-[80px] sm:h-[80px] sm:bottom-6 sm:right-6'
+        ]">
         <img :src="btnTopImg" alt="回到頂部" class="w-full h-full object-contain" />
     </button>
 </template>
+
 
 <style scoped></style>
